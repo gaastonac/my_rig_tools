fd685af00f59e82620e6d2cbaddfc5096e302803
Gaston Aguilar <64983785+gaastonac@users.noreply.github.com>
2023-05-26 12:44:25 -0600

    script for rivets on mesh with follicles

---
+++ rivet_follicle_on_mesh.py
@@ -0,0 +1,11 @@
+# rivets on mesh#
+
+for ctl in cmds.ls(sl=True):
+    # create follicle #
+    cmds.createNode('follicle')
+    cmds.pickWalk(d='up')
+    follicle = cmds.rename('{}_fl'.format(ctl))
+    cmds.connectAttr('{}Shape.outRotate'.format(follicle), '{}.r'.format(follicle), f=True)
+    cmds.connectAttr('{}Shape.outTranslate'.format(follicle), '{}.t'.format(follicle), f=True)
+
+    #test
